ba9440d6d3f862e1c3cc49276ef16f159ebddff2
giseleapreis <42588623+giseleapreis@users.noreply.github.com>
2022-07-03 17:00:27 -0300

    Create desafio1.js

---
+++ desafio1.js
@@ -0,0 +1,11 @@
+let lines = gets().split('\n');
+var N = parseInt(lines.shift());
+
+for (let i = 1; i <= N; i++) {
+
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
+
+	if (i % 2 == 0){
+	
+	print (i + "^2 = " + (i * i));}
+}
